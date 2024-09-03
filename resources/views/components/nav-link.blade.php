1396966a027d217edc584c31c91823c6f9d37307
Rafiadi28 <rafiadi28@gmail.com>
2024-09-03 13:33:56 +0700

    smknukejajar update

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,6 @@
+
+@props(['active'=> false])
+
 <a {{ $attributes }}
 class="{{ $active ? ' bg-gray-900 text-white ' :'hover:bg-gray-700 text-gray-300' }}
     rounded-md px-3 py-2 text-sm font-medium" aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a> 
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,4 +1,14 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
     <h3 class="text-xl">Ini Adalah Profil</h3>
+    <article>
+        <h2>Judul Artikel</h2>
+        <div>
+            <a href="">Rafianto Adi Kusumo</a> | 1 September 2024
+        </div>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti fugiat quam nisi totam magnam voluptates, 
+            consequatur tenetur id doloribus dignissimos cumque ea quis quos rem deserunt cum illum reprehenderit 
+            accusamus.</p>
+            <a href="">Readmore</a>
+    </article>
 </x-layout>
